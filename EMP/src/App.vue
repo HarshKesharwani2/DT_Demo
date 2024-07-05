48707630422bab03abb4f5627ef973493deddc03
Harsh Kesharwani <your_email@example.com>
2024-07-05 11:00:50 +0530

    add home, department

--- EMP/src/App.vue
+++ EMP/src/App.vue
@@ -1,15 +1,34 @@
-<script setup>
+<template>
+  <Navbar/>
+  <!-- <div id="app" class="bg-gray-100">
+    <EmployeeForm />
+  </div> -->
+</template>
+
+<script>
 import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  }
+};
 </script>
 
-<template>
-  <Navbar />
-
-  <!-- <RouterView /> -->
-</template>
-
 <style scoped>
+html, body, #app {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
 
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f9fa; /* Optional: Background color for the entire app */
+}
 </style>
